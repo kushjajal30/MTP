ade3ba56a9ac5f45347cd43d4a6e3da512c9d819
Kush Jajal <cs21m030@smail.iitm.ac.in>
2022-10-10 14:11:40 +0530

    Minor bug fix

--- REMGeneration/config.py
+++ REMGeneration/config.py
@@ -1,19 +1,19 @@
-__NDEM__ = 5
-__NCPUS__ = 4
-__output_path = 'REMS_New_Testing'
+__NDEM__ = 4
+__NCPUS__ = 25
+__output_path = 'REMS_1609'
 
 ##Terrain Generator Params
-__number_of_buildings__ = 10
+__number_of_buildings__ = 25
 __building_min_width__ = 10
 __building_min_length__ = 10
-__building_max_width__ = 60
-__building_max_length__ = 60
-__terrain_size__ = 256
+__building_max_width__ = 40
+__building_max_length__ = 40
+__terrain_size__ = 200
 __min_height__ = 15
 __max_height__ = 40
 
 #REM Generator Params
-__Ht__ = [20,40,60]
+__Ht__ = [40]
 __Hr__ = 1.5
 __fGHz__ = 0.010
 __K__ = 1.3333
