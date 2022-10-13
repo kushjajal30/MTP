83a0a13bc404f8743bda8ddbb9ab0ca5d4122a08
Kush Jajal <cs21m030@smail.iitm.ac.in>
2022-10-13 15:49:17 +0530

    Bug fixes and removed vlaidation aded mse for reconstruction

--- Modeling/ForwardMap_Pix2Pix/config.py
+++ Modeling/ForwardMap_Pix2Pix/config.py
@@ -1,10 +1,10 @@
 __bs__ = 16
-__epochs__ = 10
+__epochs__ = 30
 __dataloader_workers__ = min(25,__bs__)
-__gen_lr__ = 1e-5
-__dis_lr__ = 1e-5
+__gen_lr__ = 1e-4
+__dis_lr__ = 1e-4
 __gen_bce_lambda__ = 1
-__gen_l1_lambda__ = 10
+__gen_l1_lambda__ = 1
 __gen_l2_lambda__ = 1
 __retrain__ = False
 __model_path__ = 'SavedModels'
