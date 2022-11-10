28fc76e84dcf697c90a1eb6bf6709abeef8ad1ef
Kush Jajal <cs21m030@smail.iitm.ac.in>
2022-11-11 02:53:34 +0530

    Added mean calculator for dataloader

--- Modeling/EigenREMs.py
+++ Modeling/EigenREMs.py
@@ -5,6 +5,9 @@ import numpy as np
 
 def get_eigen_rems(data_config,nrems=2000,top_n=1):
 
+    data_config.__number_of_buildings__ = max(16,data_config.__number_of_buildings__)
+    data_config.__terrain_size__ = data_config.__terrain_size__ * 2
+
     dgen = REMInTimeDataset(data_config, nrems)
 
     intime_dataloader = DataLoader(
@@ -20,14 +23,52 @@ def get_eigen_rems(data_config,nrems=2000,top_n=1):
     for _, rem in tqdm(intime_dataloader):
         rems.append(rem.detach().numpy())
 
-    rems = np.stack(rems)
+    rems = np.concatenate([i[:, 0, :, :] for i in rems], axis=0)
+    out = []
+    for i in rems:
+        out.append(i[64:, 64:])
+        out.append(np.flip(i[64:, :64], axis=1))
+        out.append(np.flip(i[:64, 64:], axis=0))
+        out.append(np.flip(i[:64, :64], axis=(0, 1)))
 
+    rems = np.stack(out)
     svd_x = rems.reshape(nrems, -1)
-    mean_x = np.mean(svd_x, axis=0)
-    svd_x_centered = svd_x - mean_x
 
-    U, S, V = np.linalg.svd(svd_x_centered)
+    U, S, V = np.linalg.svd(svd_x)
 
     top_n_components = V[:top_n].reshape(top_n,rems.shape[1],rems.shape[2])
 
+    data_config.__terrain_size__ = data_config.__terrain_size__ / 2
+
     return top_n_components
+
+def get_mean_rem(data_config,nrems=2000):
+    data_config.__number_of_buildings__ = max(16, data_config.__number_of_buildings__)
+    data_config.__terrain_size__ = data_config.__terrain_size__ * 2
+
+    dgen = REMInTimeDataset(data_config, nrems)
+
+    intime_dataloader = DataLoader(
+        dgen,
+        batch_size=25,
+        drop_last=True,
+        shuffle=True,
+        num_workers=28
+    )
+
+    rems = []
+
+    for _, rem in tqdm(intime_dataloader):
+        rems.append(rem.detach().numpy())
+
+    rems = np.concatenate([i[:, 0, :, :] for i in rems], axis=0)
+    out = []
+    for i in rems:
+        out.append(i[64:, 64:])
+        out.append(np.flip(i[64:, :64], axis=1))
+        out.append(np.flip(i[:64, 64:], axis=0))
+        out.append(np.flip(i[:64, :64], axis=(0, 1)))
+
+    rems = np.stack(out)
+
+    return np.mean(rems)
